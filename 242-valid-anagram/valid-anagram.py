882906fecba3b2fa5b922f2ec2c9bc2106002c83
Reeya Gupta <56750374+Reeya123@users.noreply.github.com>
2025-02-07 23:03:15 -0500

    Time: 11 ms (73.63%) | Memory: 17.6 MB (89.95%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        '''
+        Valid anagram 
+
+        Time: O(n)
+        Space: O(n) - theoretical worst case scenario
+        O(k)- theoretical Avg space complexity - k is number of unique elems 
+        O(1)Since there are at most 26 unique characters, the space required for the hashmaps is constant and does not grow with 
+        the size of the input strings.
+        '''
         hashmap_s = {} #key: unique characters in s value:Num of occurences 
         hashmap_t = {} 
         if len(s) != len(t):
@@ -17,3 +26,6 @@ class Solution:
                 hashmap_t[elem] = 1  
                 
         return hashmap_s == hashmap_t
+        '''unerthe hood, hashmap s checks for each key in t and then its values. essentially making it O(n) but looking up in 
+            hashmap is O(1) so its a total of O(n) complexity
+        '''
