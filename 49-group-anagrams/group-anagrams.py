2fdc30dfad930be96cc592f0c513b852ae92dfca
Reeya Gupta <56750374+Reeya123@users.noreply.github.com>
2025-02-09 01:15:23 -0500

    Time: 20 ms (27.32%) | Memory: 22.8 MB (5.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,4 +1,18 @@
+'''
+solution 1:
+sort every string in the list 
+hashmap - key: sorted string; value: the anagrams 
+return the hashmap values as a list 
+Time: n * m log m 
 
+solution 2: without sorting 
+count frequencies of chqaracters and save t hem as keys 
+create an array of size 26 
+add the frequency count of each element and make it a tuple 
+add to hashmap as key 
+check for each string and add as value if frequency count is same 
+
+'''
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
