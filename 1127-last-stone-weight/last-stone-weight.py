449f9b006b3747d9ec5782d42fce07836abc13f7
Reeya Gupta <56750374+Reeya123@users.noreply.github.com>
2025-02-11 23:05:13 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (90.65%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,15 +1,23 @@
 class Solution:
+    '''
+    solution:
+    []
+    #convert the aarray into a maxheap 
+    get the x and y; caalculate new values and update the array accordingly 
+    space ; O(n) for storing the heap 
+    time: O(n log k)
+    '''
     def lastStoneWeight(self, nums: List[int]) -> int:
-        maxheap = [-stone for stone in nums]
-        heapq.heapify(maxheap)
+        maxheap = [-stone for stone in nums] # O(n) space
+        heapq.heapify(maxheap) #O(n) time
 
         #Keepdoing it until we have 1 or no stones left 
         while len(maxheap) > 1:
-            y = -heapq.heappop(maxheap)
-            x = -heapq.heappop(maxheap)
-            
+            y = -heapq.heappop(maxheap) #O(log k) time
+            x = -heapq.heappop(maxheap) #O(log k) time
+
             if x != y:
-                heapq.heappush(maxheap, -(y - x))
-                
-        
-        return -maxheap[0] if maxheap else 0
+                heapq.heappush(maxheap, -(y - x)) #O(log k) time
+
+
+        return -maxheap[0] if maxheap else 0 
