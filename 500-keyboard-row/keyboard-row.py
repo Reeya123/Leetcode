21315015867c64ea949934253ef4f27b04af1701
Reeya Gupta <56750374+Reeya123@users.noreply.github.com>
2025-03-12 19:41:02 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (32.01%) - LeetSync

--- 500-keyboard-row/keyboard-row.py
+++ 500-keyboard-row/keyboard-row.py
@@ -1,38 +1,20 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        row1 = "qwertyuiop"
-        row2 = "asdfghjkl"
-        row3 = "zxcvbnm"
+        row1 = set("qwertyuiop") ##set uses hashing so lookup is O(1) while string lookup is O(m) - m is len of teh string 
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
         res = []
         for word in words:
-            lower_word = word.lower() #converts every letter to lowercase 
+            lower_word = set(word.lower()) #converts every letter to lowercase 
 
             #determine which row first letter belongs to 
-            if lower_word[0] in row1:
-                #logic to check if all the letters in the word belong to same row 
-                for letter in lower_word:
-                    if letter not in row1:
-                        break
-                else:
-                    res.append(word)
-                
-            elif lower_word[0] in row2:
-                for letter in lower_word:
-                    if letter not in row2:
-                        break
-                else:
-                    res.append(word)
-                        
-
-            elif lower_word[0] in row3:
-                for letter in lower_word:
-                    if letter not in row3:
-                        break
-                else:
-                    res.append(word)
-                        
-            else:
-                #invalid char
-                continue
+            if lower_word.issubset(row1) or lower_word.issubset(row2) or lower_word.issubset(row3):
+                res.append(word)
         
         return res
+
+        '''
+to use issubset, both the words have to be a set - set(word).issubset(set(bigword))
+It returns True if all elements of the first set are present in the second set, and False otherwise. 
+It can be used with the <= operator as a shortcut.
+'''
